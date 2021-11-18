f016dc211a05b450045a940bff47d57503288c51
user <stokdam@gmail.com>
2021-11-18 18:58:17 +0100

    mysql database crp storage

--- Host/puf_test.py
+++ Host/puf_test.py
@@ -2,15 +2,15 @@ import serial
 import numpy
 import matplotlib.pyplot as plt
 import sys
+import mysql.connector
 
 
-def run_puf(port, challenge, n=1000):
-    block_size = 4
+def run_puf(port, challenge, mysql_cnx, n=1000):
     cursor = ['-', '\\', '|', '/']
     ser = serial.Serial(port)  # open serial port
     ser.rtscts = True
     ser.baudrate = 115200    
-    n_osc = numpy.zeros(10000)
+    n_osc = numpy.zeros(10000, dtype=int)
     valid_res=0
     ser.timeout = 2.0
     for i in range(0, n):
@@ -20,19 +20,47 @@ def run_puf(port, challenge, n=1000):
         result = int.from_bytes(puf_resp[4:6], 'little')
         if (final_value == 0xAAAAAAAA or final_value == 0x55555555):
             valid_res+=1;    
-            n_osc[result//block_size] += 1
-            if (result % block_size >= block_size//2):
-                n_osc[(result+block_size//2)//block_size] += 1
-        print("  ["+cursor[(i//10)%4] + "]", end='\r')
+            n_osc[result] += 1
     ser.close()
+    
+    cursor = mysql_cnx.cursor()
+    
+    add_run = ("INSERT INTO PUF_runs "
+               "(secube, challenge, valid_responses) "
+               "VALUES (%s, %s, %s)")
+               
+    data_run = (port, bytearray(challenge), valid_res)
+    cursor.execute(add_run, data_run)
+    
+    run_id = cursor.lastrowid
+    
+    for i in range(0, 10000):
+        if (n_osc[i] > 0):
+            add_results = ("INSERT INTO PUF_results "
+               "(runID, response, n_occurrences) "
+               "VALUES (%s, %s, %s)")
+               
+            data_results = (run_id, i, int(n_osc[i]))
+            cursor.execute(add_results, data_results)
+    
+    mysql_cnx.commit()
+    cursor.close();
+    
     return n_osc, valid_res 
+    
 
-challenge = [0, 0xaa, 0, 0, 0, 1, 0, 0]
+cnx = mysql.connector.connect(user='user', password='cc5XunxY',
+                              host='127.0.0.1',
+                              database='PUF_CRPs')
+
+challenge = [65, 65, 3, 0, 65, 65, 65, 65]
 challenge2 = [0, 0xaa, 0xff, 0xff, 0xff, 1, 0, 0]
-n_osc, valid_res = run_puf(sys.argv[1], challenge, 10000)
+n_osc, valid_res = run_puf(sys.argv[1], challenge, cnx, 1000)
 n_osc = n_osc/valid_res
-n_osc2, valid_res2 = run_puf(sys.argv[1], challenge2, 10000)
+n_osc2, valid_res2 = run_puf(sys.argv[1], challenge2, cnx, 1000)
 n_osc2 = n_osc2/valid_res2
+
+cnx.close()
 #valid_res = len(n_osc)
 #sigma = numpy.std(n_osc)
 #avg = numpy.average(n_osc)
@@ -46,12 +74,3 @@ print("Distance: " + str(dist))
 plt.plot(range(0,10000), n_osc)
 plt.plot(range(0,10000), n_osc2)
 plt.show()
-
-# ser.write(b'puff' + bytearray(challenge) + bytearray(4))
-# puf_resp = ser.read(16)
-# final_value = puf_resp[0:4].hex()
-# avg = int.from_bytes(puf_resp[4:6], 'little')
-
-#print("Final value: " + final_value)
-#print("Number of oscillation: " + str(avg))
-#print(puf_resp.hex())
