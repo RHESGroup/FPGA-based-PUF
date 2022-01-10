ea2e09f5a5d2be416f4fccd6df3dc06089ac513d
stokdam <stokdam@gmail.com>
2022-01-10 23:34:04 +0100

    serial endianess

--- BOOT/Src/stlink_serial.s
+++ BOOT/Src/stlink_serial.s
@@ -5,5 +5,6 @@
 st_serial:                     // Function "myadd" entry point.
      .fnstart
      BKPT   // Function arguments are in R0 and R1. Add together and put the result in R0.
+     rev r0, r0
      bx       lr           // Return by branching to the address in the link register.
      .fnend
